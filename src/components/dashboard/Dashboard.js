9658d8db4330b8cdf62bdf31a2f687ee0f91d9d7
ayesha nadeem <ayeshanadeem291@gmail.com>
2019-10-18 15:33:31 +0100

    On branch master

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,15 @@
 
 import React, { Component } from 'react'
-import Notifications from '/Notifications'
+import Notifications from './Notifications'
+import ProjectList from '../projects/ProjectList'
 export default class Dashboard extends Component {
     render() {
         return (
 
             <div className= "dasboard container">
                 <div className="row">
-                    <div className="col s12 m6"></div>
-                    <div className="col s12 m6 offset-m1"></div>
+                    <div className="col s12 m6"><ProjectList/></div>
+                    <div className="col s12 m5 offset-m1"></div>
                 <Notifications/>
                 </div>
                 
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -4,11 +4,40 @@ export default class ProjectList extends Component {
     render() {
         return (
             <div className="project-list section">
-             <div className="card z-depth-0 project-summary"></div>   
-            <div className="">
+             <div className="card z-depth-0 project-summary">  
+            <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title</span>
+            <p>Posted by net ninja</p>
+            <p className="grey-text">
+             3rd September, 2 am
+            </p>
+            </div>
+            </div>
 
+            
+    <div className="card z-depth-0 project-summary">   
+        <div className="card-content grey-text text-darken-3">
+    <span className="card-title">Project Title</span>
+<p>Posted by net ninja</p>
+<p className="grey-text">
+3rd September, 2 am
+</p>
+</div>
+</div> 
+
+
+
+             <div className="card z-depth-0 project-summary">  
+            <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title</span>
+            <p>Posted by net ninja</p>
+            <p className="grey-text">
+             3rd September, 2 am
+            </p>
             </div>
-            </div>
+            </div> 
+            </div> 
+           
         )
     }
 }
